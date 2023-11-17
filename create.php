0ea985a2e5886a796e00fe97066b6f8c25d8bcef
adriano-ramoss <adrianoramos2401@gmail.com>
2023-11-16 21:29:43 -0300

    Deploy

--- create.php
+++ create.php
@@ -8,7 +8,7 @@ include_once("templates/header.php");
         <?php include_once("templates/backbtn.php") ?>
         <h1 id="main-title">Criar Contato</h1> 
 
-        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+        <form id="create-form" action="/config/process.php" method="POST">
             <input type="hidden" name="type" value="create"> 
 
             <div class="form-group mt-2">
